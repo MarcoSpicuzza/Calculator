bf8c322cc5ceb62b5ec5fa506703027279c36e39
MarcoSpicuzza00 <marcospicuzza00@gmail.com>
2022-01-29 15:26:36 +0100

    testing JavaScript logic since it is bugged on chrome

--- script.js
+++ script.js
@@ -1,63 +1,71 @@
-let number1 = 0;
-let number2 = 0;
+const calcButtons = document.querySelectorAll("button");
+const previousOperand = document.querySelector(".previousOperand");
+const currentOperand = document.querySelector(".currentOperand");
 
-function add(add){
-    number1 = window.prompt("Add|Insert number1");
-    number2 = window.prompt("Add|Insert number2");
-    add = Number(number1) + Number(number2);
-    alert(add);
-}
-// add(add);
+let displayNumber = "";
 
-function subtract(subtract){
-    number1 = window.prompt("Add|Insert number1");
-    number2 = window.prompt("Add|Insert number2");
-    subtract = Number(number1) - Number(number2);
-    alert(subtract);
-}
-// subtract(subtract);
-
-function multiply(multiply){
-    number1 = window.prompt("Add|Insert number1");
-    number2 = window.prompt("Add|Insert number2");
-    multiply = Number(number1) * Number(number2);
-    alert(multiply);
-}
-// multiply(multiply);
-
-function divide(divide){
-    number1 = window.prompt("Add|Insert number1");
-    number2 = window.prompt("Add|Insert number2");
-    divide = Number(number1) / Number(number2);
-    alert(divide);
+function add(n1, n2){
+    return n1 + n2;
 }
 
-function operate(){
-    selection = window.prompt("Choose a number for the operation. \n 1: add \n 2: subtract \n 3: multiply \n 4: divide ");
-    switch(selection){
-        case "1":
-            if(selection = 1){
-                add(add);
-            }
-        break;
-
-        case "2":
-            if(selection = 2){
-                subtract(subtract);
-            }
-        break;
-
-        case "3":
-            if(selection = 3){
-                multiply(multiply);
-            }
-        break;
-
-        case "4":
-            if(selection = 3){
-                divide(divide);
-            }
-        break;
-    }
+function subtract(n1, n2){
+    return n1 - n2;
 }
-operate();
+
+function multiply(n1, n2){
+    return n1 * n2;
+}
+
+function divide(n1, n2){
+    return n1 / n2;
+}
+
+function collectData(e) {
+  let data = "";
+  if (e.key === undefined) {
+      data = this.id;
+  } else {
+      data = e.key;
+  }
+  
+  switch(data) {
+      case '0':
+          displayNumber = displayNumber + '0';
+          break;
+      case '9':
+          displayNumber = displayNumber + '9';
+          break;
+  }
+    currentOperand.textContent = displayNumber;
+}
+
+// // // // // // // // function operate(){
+// // // // // // // //     selection = window.prompt("Choose a number for the operation. \n 1: add \n 2: subtract \n 3: multiply \n 4: divide ");
+// // // // // // // //     switch(selection){
+// // // // // // // //         case "1":
+// // // // // // // //             if(selection = 1){
+// // // // // // // //                 add(add);
+// // // // // // // //             }
+// // // // // // // //         break;
+
+// // // // // // // //         case "2":
+// // // // // // // //             if(selection = 2){
+// // // // // // // //                 subtract(subtract);
+// // // // // // // //             }
+// // // // // // // //         break;
+
+// // // // // // // //         case "3":
+// // // // // // // //             if(selection = 3){
+// // // // // // // //                 multiply(multiply);
+// // // // // // // //             }
+// // // // // // // //         break;
+
+// // // // // // // //         case "4":
+// // // // // // // //             if(selection = 3){
+// // // // // // // //                 divide(divide);
+// // // // // // // //             }
+// // // // // // // //         break;
+// // // // // // // //     }
+// // // // // // // // }
+
+calcButtons.forEach(calcButton => calcButton.addEventListener("click", collectData))
